51aea29ae666c8e6c19922235a9fe89c585f7efa
Danielntda <danielntda@gmail.com>
2018-01-08 16:35:31 +0800

    gui

---
+++ CAOSProject/CAOSProject/GUI.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CAOSProject
+{
+    public partial class GUI : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
